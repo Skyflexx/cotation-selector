1bc656b70a38d3b8820939a49b935be35e8aa742
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-02-26 10:52:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 4
+let count = 3
 
 function increment() {
     count += 1
     if(count > 9){
-        count = 4
+        count = 3
     }
     countEl.textContent = count
     
@@ -14,6 +14,6 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
-    countEl.textContent = 4
-    count = 4
+    countEl.textContent = 3
+    count = 3
 }
